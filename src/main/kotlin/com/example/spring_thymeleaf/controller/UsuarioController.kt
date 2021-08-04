d9c42900c2a45d18f27e0e30eebc7b9f965ed03c
GERARDO MORALES <moralesgerardomgz@outlook.com>
2021-08-04 00:56:05 -0500

    sereliza mejoras en la validacion y de sessiones con spring y fotmularios

--- src/main/kotlin/com/example/spring_thymeleaf/controller/UsuarioController.kt
+++ src/main/kotlin/com/example/spring_thymeleaf/controller/UsuarioController.kt
@@ -6,19 +6,33 @@ import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.SessionAttributes
+import org.springframework.web.bind.support.SessionStatus
 import javax.validation.Valid
 
 @Controller
+@SessionAttributes("usuario")
 class UsuarioController {
+
+    // @SessionAttributes("usuario") se guarda en una sesion y se mantiene Vivos entre sesiones
 
     @GetMapping("", "/", "/registro")
     fun formularioUsuario(vista: Model): String {
-        vista.addAttribute("usuario", Usuario())
+        val usuario = Usuario()
+        usuario.nombreUsuario = "Israel Morales ññÑíáóú"
+        usuario.edad = 22
+        usuario.id = 233_323_349
+        vista.addAttribute("usuario", usuario)
         return "form_usuario"
     }
 
     @PostMapping("/proceso")
-    fun procesoFormUsuario(@Valid usuario: Usuario, resultado: BindingResult, vista: Model): String {
+    fun procesoFormUsuario(
+        @Valid usuario: Usuario,
+        resultado: BindingResult,
+        vista: Model,
+        status: SessionStatus
+    ): String {
         //El BindingResult toma las validaciones realizadas y aplicadas en el formulario
 
         if (resultado.hasErrors()) {
@@ -30,15 +44,13 @@ class UsuarioController {
             //se comneta este codigfo con el fin de realizar esta validacion de error desde
             // la parte del formulario html llamado form_usuario
             */
-
-
-
-
             return "form_usuario"
         }
 
         vista.addAttribute("usuario", usuario)
         vista.addAttribute("titulo", "Resultado de la informcion")
+        status.setComplete() //Se elimina la seccion levantada en para esta peticion
+        //la session levantada es @SessionAttributes("usuario") creada desde la clase controller
         return "proceso_form"
     }
 
--- src/main/kotlin/com/example/spring_thymeleaf/model/Usuario.kt
+++ src/main/kotlin/com/example/spring_thymeleaf/model/Usuario.kt
@@ -2,9 +2,13 @@ package com.example.spring_thymeleaf.model
 
 import org.springframework.stereotype.Component
 import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 @Component
 class Usuario {
+
+    final var id: Long? = null
+
     @NotEmpty
     final var nombreUsuario: String? = null
 
@@ -17,12 +21,22 @@ class Usuario {
     @NotEmpty
     final var contrasenia: String? = null
 
+    @NotNull
+    final var edad: Int? = null
+
     constructor()
 
-    constructor(nombreUsuario: String?, apellidoUsuario: String?, correoUsuario: String?, contrasenia: String?) {
+    constructor(
+        nombreUsuario: String?,
+        apellidoUsuario: String?,
+        correoUsuario: String?,
+        contrasenia: String?,
+        edad: Int?
+    ) {
         this.nombreUsuario = nombreUsuario
         this.apellidoUsuario = apellidoUsuario
         this.correoUsuario = correoUsuario
         this.contrasenia = contrasenia
+        this.edad = edad
     }
 }
